40757ba1a7c3ec4a97b1a0a1c3150528e34b3221
セミ141 <whompsfortress141@gmail.com>
2025-09-16 00:51:35 +0900

    修正版コードを追加

--- memo.rb
+++ memo.rb
@@ -1,23 +1,35 @@
 require 'csv'
 
+puts "ファイル名を入力してください"
+filename = gets.chomp
+
 puts "1: 新規作成 2: 既存ファイル編集"
 choice = gets.chomp
 
 #入力チェック
 until choice == "1" || choice == "2"
-    puts "1か2を入力してください～！"
-    choice = gets.chomp
+  puts "1か2を入力してください"
+  choice = gets.chomp
 end
 
-puts "メモしたい内容をここに入力してください～！"
-STDOUT.flush # 案内強制表示
-memo = gets.chomp #　入力待ち状態
+puts "メモしたい内容を入力してください(複数行OK、終了する場合は 'END' を入力)"
+
+memo_lines = []
+while true
+  line = gets.chomp
+  break if line == "END"
+  memo_lines << line
+end
 
 mode = choice == "1" ? "w" : "a"
-CSV.open("memo.csv", mode) do |csv|
-    csv << [memo] #　入力した内容を1行として書き込む
+CSV.open(filename, mode) do |csv|
+  memo_lines.each do |line|
+    csv << [line]
+  end
 end
 
-puts "書き込み完了～！"
+puts "書き込み完了"
 
-puts "入力確認: #{memo}"
+puts "入力確認"
+
+memo_lines.each { |line| puts line}
