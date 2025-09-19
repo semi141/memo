2192fb859ab6665dd5397b854f107aafd713fc46
セミ141 <whompsfortress141@gmail.com>
2025-09-19 16:49:27 +0900

    課題1再提出：Ctrl+D対応のメモアプリ

--- memo.rb
+++ memo.rb
@@ -1,31 +1,25 @@
-# Windows環境では Control+D が使えないため、入力終了は 'END' としています。
-# それ以外の実装要件は提出時の課題仕様に沿っています。
-
 require 'csv'
-
-puts "ファイル名を入力してください"
-filename = gets.chomp
-
-
-# 入力したファイル名に拡張子がなければ .csv を付ける
-filename += ".csv" unless filename.end_with?(".csv")
-
 
 puts "1: 新規作成 2: 既存ファイル編集"
 choice = gets.chomp
 
 #入力チェック
 until choice == "1" || choice == "2"
-  puts "1か2を入力してください"
+  puts "1か2を入力してください(半角)"
   choice = gets.chomp
 end
 
-puts "メモしたい内容を入力してください(複数行OK、終了する場合は 'END' を入力)"
+puts "ファイル名を入力してください"
+filename = gets.chomp
+
+# 入力したファイル名に拡張子がなければ .csv を付ける
+filename += ".csv" unless filename.end_with?(".csv")
+
+puts "メモしたい内容を入力してください(複数行OK、終了する場合は 'Ctrl+D' を入力)"
 
 memo_lines = []
-while true
-  line = gets.chomp
-  break if line == "END"
+while line = gets
+  line.chomp!
   memo_lines << line
 end
 
